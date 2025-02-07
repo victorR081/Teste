0144c1b080c7065a1fd85d0d5e5c0719288aa167
victorR081 <victorr0801@gmail.com>
2025-02-07 12:38:31 -0300

    Update and rename inverter_string.py to Digitos espaçados.py

--- inverter_string.py
+++
@@ -1,9 +0,0 @@
-def inverter_string(s):
-    string_invertida = ""
-    for char in s:
-        string_invertida = char + string_invertida
-    return string_invertida
-
-# Entrada do usuário
-texto = input("Informe uma string: ")
-print(f"String invertida: {inverter_string(texto)}")
